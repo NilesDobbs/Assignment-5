a29fefce5992189fe025d0a97a113e9d0201eebf
NilesDobbs <83557367+NilesDobbs@users.noreply.github.com>
2022-01-22 13:55:44 -0500

    Update CustomProgram.java

--- CustomProgram.java
+++ CustomProgram.java
@@ -3,7 +3,9 @@ package com.assignmentFive;
 public class CustomProgram {
 	private static void addNewApplicants(CustomList<ApplicationInfo> applicants) {
 		for (int i = 0; i < 15; i++) {
-			ApplicationInfo applicant = new ApplicationInfo("Elon" + (i+1), "Musk" + (i+1));
+			
+			//For each index, add a new member (Elon Musk #) to our list, we then instantiate in main.
+			ApplicationInfo applicant = new ApplicationInfo("Elon " + (i+1), "Musk " + (i+1));
 			applicants.add(applicant);
 			System.out.println(applicant);
 		}
@@ -11,12 +13,19 @@ public class CustomProgram {
 	
 	public static void main(String[] args) {
 		CustomList<ApplicationInfo> applicant = new CustomArrayList<>();
+		//Get the size and index location of the first applicant
 		System.out.println("# of starting applicants: " + applicant.getSize());
 		System.out.println("1st Applicant " + applicant.get(0));
-		applicant.add(new ApplicationInfo("Member", " "));
-		System.out.println("# of new applicants: " + applicant.getSize());
-		addNewApplicants(applicant);
-		System.out.println("# of total applicants: " + applicant.getSize());
+		
+		///Return the 5th applicant in the index.
 		System.out.println("Applicant #5: " + applicant.get(4));
+		
+		//Add
+		applicant.add(new ApplicationInfo("Member", " "));
+		System.out.println("Number of new applicants: " + applicant.getSize());
+		addNewApplicants(applicant);
+		
+		System.out.println("Number of applicants: " + applicant.getSize());
+	
 	}
 }
